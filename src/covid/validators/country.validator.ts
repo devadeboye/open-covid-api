30925b9acd76662a0c5cda5e2a536c07a162fb12
devadeboye <emmanueladeboye2017@gmail.com>
2021-12-28 20:54:36 +0100

    improve validation

--- src/covid/validators/country.validator.ts
+++ src/covid/validators/country.validator.ts
@@ -1,8 +1,13 @@
 import * as Joi from 'joi';
 import { CountryEnum } from 'src/utils/enums/country.enum';
+import { capitalize } from 'src/utils/fuctions/capitalize.function';
 
 export const countryNameValidator = Joi.string()
   .valid(...Object.values(CountryEnum))
+  .insensitive()
+  .custom((value, helpers) => {
+    return capitalize(value);
+  })
   .required()
   .messages({
     'any.required': `please provide a valid country name as parameter`,
--- src/covid/validators/state.validator.ts
+++ src/covid/validators/state.validator.ts
@@ -1,10 +1,15 @@
 import * as Joi from 'joi';
 import { StateEnum } from 'src/utils/enums/state.enum';
+import { capitalize } from 'src/utils/fuctions/capitalize.function';
 
 export const stateNameValidator = Joi.string()
   .valid(...Object.values(StateEnum))
   .insensitive()
+  .custom((value, helpers) => {
+    return capitalize(value);
+  })
   .required()
   .messages({
     'any.required': `please provide a valid state name as parameter`,
+    //TODO: add message for when value is not one of the valid values
   });
