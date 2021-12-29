9304977aaa2cac76acf48dbc0582a6af5fce5cfc
devadeboye <emmanueladeboye2017@gmail.com>
2021-12-29 18:21:05 +0100

    Update documentation

--- src/covid/controllers/country.controller.ts
+++ src/covid/controllers/country.controller.ts
@@ -9,12 +9,31 @@ import { CountryEnum } from 'src/utils/enums/country.enum';
 import { StateEnum } from 'src/utils/enums/state.enum';
 import { CountryService } from '../services/country.service';
 import { countryNameValidator } from '../validators/country.validator';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiParam,
+} from '@nestjs/swagger';
+import { Country } from '../models/country.model';
 
 @Controller({ version: '1' })
 export class CountryController {
   constructor(private readonly countryService: CountryService) {}
 
   @Get('covid/country/:name')
+  @ApiParam({
+    name: 'name',
+    enum: Object.values(CountryEnum),
+    description:
+      'This is the name of the country you want to get covid-19 information about',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'record found for country',
+    type: Country,
+  })
   async getSummaryByCountry(@Param('name') country: CountryEnum) {
     try {
       country = await countryNameValidator.validateAsync(country);
--- src/covid/controllers/state.controller.ts
+++ src/covid/controllers/state.controller.ts
@@ -10,12 +10,37 @@ import { StateEnum } from 'src/utils/enums/state.enum';
 import { StateService } from '../services/state.service';
 import { countryNameValidator } from '../validators/country.validator';
 import { stateNameValidator } from '../validators/state.validator';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiParam,
+} from '@nestjs/swagger';
+import { State } from '../models/state.model';
 
 @Controller({ version: '1' })
 export class StateController {
   constructor(private readonly stateService: StateService) {}
 
   @Get('covid/:country/state/:name')
+  @ApiParam({
+    name: 'country',
+    enum: Object.values(CountryEnum),
+    description:
+      'This is the name of the country you want to get covid-19 information about',
+  })
+  @ApiParam({
+    name: 'name',
+    enum: Object.values(StateEnum),
+    description:
+      'This is the name of the state you want to get covid-19 information about',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'record found for state',
+    type: State,
+  })
   async getStatisticsByState(
     @Param('country') country: CountryEnum,
     @Param('name') name: StateEnum,
@@ -30,6 +55,17 @@ export class StateController {
   }
 
   @Get('covid/:country/state/mortality/highest')
+  @ApiParam({
+    name: 'country',
+    enum: Object.values(CountryEnum),
+    description:
+      'This is the name of the country you want to get covid-19 information about',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'record found',
+    type: State,
+  })
   async stateWithHighestMortality(@Param('country') country: CountryEnum) {
     try {
       country = await countryNameValidator.validateAsync(country);
@@ -40,6 +76,17 @@ export class StateController {
   }
 
   @Get('covid/:country/state/mortality/lowest')
+  @ApiParam({
+    name: 'country',
+    enum: Object.values(CountryEnum),
+    description:
+      'This is the name of the country you want to get covid-19 information about',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'record found',
+    type: State,
+  })
   async stateWithLowestMortality(@Param('country') country: CountryEnum) {
     try {
       country = await countryNameValidator.validateAsync(country);
@@ -50,6 +97,17 @@ export class StateController {
   }
 
   @Get('covid/:country/state/active-case/highest')
+  @ApiParam({
+    name: 'country',
+    enum: Object.values(CountryEnum),
+    description:
+      'This is the name of the country you want to get covid-19 information about',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'record found',
+    type: State,
+  })
   async stateWithHighestActiveCases(@Param('country') country: CountryEnum) {
     try {
       country = await countryNameValidator.validateAsync(country);
@@ -60,6 +118,17 @@ export class StateController {
   }
 
   @Get('covid/:country/state/confirmed-case/highest')
+  @ApiParam({
+    name: 'country',
+    enum: Object.values(CountryEnum),
+    description:
+      'This is the name of the country you want to get covid-19 information about',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'record found',
+    type: State,
+  })
   async stateWithHighestConfirmedCases(@Param('country') country: CountryEnum) {
     try {
       country = await countryNameValidator.validateAsync(country);
