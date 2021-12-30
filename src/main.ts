b6a20094812f5e0eed203a0d6d5852239f8c43fc
devadeboye <emmanueladeboye2017@gmail.com>
2021-12-30 18:25:46 +0100

    pick port from .env

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { VersioningType } from '@nestjs/common';
+import { Logger, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { envConfiguration } from './utils/config/env.configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,9 +11,11 @@ async function bootstrap() {
     type: VersioningType.URI,
   });
   app.setGlobalPrefix('api');
+  const config = app.get(ConfigService);
+  const port = process.env.PORT || config.get(envConfiguration.PORT);
 
   // setup swagger
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Open Covid-19 APIs')
     .setDescription(
       'This project contains open APIs with which you can get covid-19 information for supported countries',
@@ -19,9 +23,11 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('covid')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(port, () => {
+    Logger.log(`listening on port ${port}`);
+  });
 }
 bootstrap();
